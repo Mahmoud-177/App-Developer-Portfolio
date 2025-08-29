30ff50a7b3af3fe48e3d32ff02b6b72de700a5a1
Mahmoud Deebo <hodadeebo16112008@gmail.com>
2025-08-30 00:23:27 +0300

    The portfolio for the app dev

---
+++ JS/main.js
@@ -0,0 +1,66 @@
+const sections = document.querySelectorAll("section");
+const headerLinks = document.querySelectorAll("header .nav-links a");
+
+function setActiveLink(links, id) {
+    links.forEach(link => {
+        if (link.getAttribute("href") === `#${id}`) {
+        link.classList.add("active");
+        } else {
+        link.classList.remove("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", () => {
+    let currentSection = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+        currentSection = section.getAttribute("id");
+        }
+    });
+
+    if (currentSection) {
+        setActiveLink(headerLinks, currentSection);
+    }
+});
+const scrollBtn = document.getElementById("top");
+const heroSection = document.getElementById("hero");
+
+window.addEventListener("scroll", () => {
+    const heroBottom = heroSection.offsetTop + heroSection.offsetHeight;
+    if (window.scrollY > heroBottom) {
+    scrollBtn.classList.add("show");
+    } else {
+    scrollBtn.classList.remove("show");
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+    });
+});
+const body = document.body
+const themeToggle = document.getElementById("themeToggle")
+const themeIcon = themeToggle.querySelector("i")
+
+const currentTheme = localStorage.getItem("theme")
+if(currentTheme === 'light'){
+    body.classList.add('light')
+    themeIcon.classList.replace('fa-sun', 'fa-moon')
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme')
+    if(body.classList.contains('dark-theme')){
+        themeIcon.classList.replace('fa-sun', 'fa-moon')
+    } else {
+        themeIcon.classList.replace('fa-moon', 'fa-sun')
+        localStorage.setItem('theme','dark')
+    }
+})
---
+++ JS/projects.js
@@ -0,0 +1,7 @@
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});
