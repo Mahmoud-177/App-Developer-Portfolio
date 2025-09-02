80ebc59e600604d6f6a59698d28742f6e4cabfca
Mahmoud Deebo <hodadeebo16112008@gmail.com>
2025-09-02 15:21:08 +0300

    Added Effects

--- JS/projects.js
+++ JS/projects.js
@@ -1,7 +1,17 @@
-const projectCards = document.querySelectorAll('.project-card');
+const projects = document.querySelectorAll(".project-card");
+const overlay = document.querySelector(".overlay");
+const details = document.querySelectorAll(".project-details"); // هنا من الـ document كله
 
-projectCards.forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.toggle('flipped');
+projects.forEach(project => {
+    const btn = project.querySelector(".btn");
+    const num = btn.getAttribute("data-num"); // جاي من HTML data-btn
+
+    btn.addEventListener("click", () => {
+        details[num - 1].classList.toggle("shown");
+        overlay.classList.toggle("shown");
     });
+    overlay.addEventListener("click", _ => {
+        details.forEach((d) => d.classList.remove("shown"))
+        overlay.classList.remove("shown")
+    })
 });
